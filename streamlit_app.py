a25f38071c856b6a4fd9db713718656fc1f83d82
Alexandreveaux <alexandre.veaux@sbi-group.com>
2025-04-28 15:25:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,8 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
+        smoothiefroot_response=requests.get("https://my.smoothiefroot.com/api/watermelon"
+        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=true)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
@@ -35,6 +37,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered'+','+name_on_order+'!', icon="✅")
 Import requests
-smoothiefroot_response=requests.get("https://my.smoothiefroot.com/api/watermelon"
-#st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=true)
