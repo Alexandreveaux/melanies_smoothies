09080766be6a0ec6f20f939e80ba5ffef79323dc
Alexandreveaux <alexandre.veaux@sbi-group.com>
2025-04-29 10:01:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,3 @@ if ingredients_list:
         st.success('Your Smoothie is ordered'+','+name_on_order+'!', icon="✅")
 
 
-smoothiefroot_response = requests.get("https://my.SMOOTHIEFROOT.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response)
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
